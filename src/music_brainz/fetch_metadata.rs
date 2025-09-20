740ac26831b0937d40b16ce87005856ff48b2f2b
Seva Zaikov <seva.zaikov@gmail.com>
2025-09-19 21:11:12 -0700

    parse response metadata

--- src/music_brainz/fetch_metadata.rs
+++ src/music_brainz/fetch_metadata.rs
@@ -16,6 +16,44 @@ pub struct Release {
     pub title: String,
     pub date: Option<String>,
     pub country: Option<String>,
+    #[serde(rename = "release-group")]
+    pub release_group: Option<ReleaseGroup>,
+    #[serde(rename = "cover-art-archive")]
+    pub cover_art_archive: Option<CoverArtArchive>,
+    pub media: Option<Vec<Medium>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ReleaseGroup {
+    pub id: String,
+    pub title: Option<String>,
+    #[serde(rename = "primary-type")]
+    pub primary_type: Option<String>
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CoverArtArchive {
+    pub artwork: bool,
+    pub count: u32,
+    pub front: bool,
+    pub back: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Medium {
+    pub format: Option<String>,          // "CD", etc.
+    pub position: Option<u32>,
+    #[serde(rename = "track-count")]
+    pub track_count: Option<u32>,
+    pub tracks: Option<Vec<Track>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Track {
+    pub id: String,
+    pub number: Option<String>,          // "1", "2", …
+    pub title: Option<String>,
+    pub length: Option<u32>,             // ms
 }
 
 #[derive(Debug)]
@@ -63,10 +101,10 @@ impl MusicBrainzClient {
         id: &str,
         includes: &[&str],
     ) -> Result<MusicBrainzResponse, MusicBrainzError> {
-        let mut url = format!("https://musicbrainz.org/ws/2/discid/{}", id);
+        let mut url: String = format!("https://musicbrainz.org/ws/2/discid/{}", id);
 
         if !includes.is_empty() {
-            url.push_str(&format!("&inc={}", includes.join("+")));
+            url.push_str(&format!("?inc={}", includes.join("+")));
         }
 
         let response = self
@@ -86,10 +124,113 @@ impl MusicBrainzClient {
 
         println!("MusicBrainzId: {id}");
 
-        let includes: Vec<&str> = vec![];
+        let includes: Vec<&str> = vec![
+            "recordings",
+        ];
 
         let result = self.lookup_by_disc_id(&id, &includes);
 
-        println!("{:#?}", result);
+        match result {
+            Ok(response) => {
+                let album_data = self.parse_metadata(&response);
+
+                println!("{:#?}", album_data);
+            }
+            Err(error) => {
+                println!("{:#?}", error);
+            },
+        }
+    }
+
+    fn parse_metadata(&self, response: &MusicBrainzResponse) -> Option<Album> {
+        let Some(releases) = &response.releases else {
+            return None;
+        };
+
+        if releases.is_empty() {
+            return None;
+        }
+
+        let Some(release) = releases.get(0) else {
+            return None;
+        };
+
+        let title = release.title.clone();
+        let country = release.country.clone().unwrap_or("unknown".to_string());
+
+        let Some(cd_media) = self.find_cd_media(&release.media) else {
+            return None;
+        };
+
+        let Some(tracks) = &cd_media.tracks else {
+            return None;
+        };
+        
+        let album_tracks = self.parse_album_tracks(tracks);
+
+        let album = Album { title, country, tracks: album_tracks };
+
+        Some(album)
+    }
+
+    /// for now, find the first CD media and return it
+    fn find_cd_media<'a>(&self, media: &'a Option<Vec<Medium>>) -> Option<&'a Medium> {
+        let Some(mediums) = media else {
+            return None;
+        };
+
+        for medium in mediums {
+            if let Some(medium_format) = &medium.format {
+                if medium_format == "CD" {
+                    return Some(medium);
+                }
+            }
+        }
+
+        return None;
+    }
+
+    fn parse_album_tracks(&self, tracks: &Vec<Track>) -> Vec<AlbumTrack> {
+        let mut result: Vec<AlbumTrack> = Vec::new();
+
+        for track in tracks {
+            if let Some(album_track) = AlbumTrack::new(track) {
+                result.push(album_track);
+            }
+        }
+
+        result
+    }
+}
+
+#[derive(Debug)]
+pub struct Album {
+    pub title: String,
+    pub country: String,
+    pub tracks: Vec<AlbumTrack>,
+}
+
+#[derive(Debug)]
+pub struct AlbumTrack {
+    pub num: u32,
+    pub title: String,
+    pub len: u32,
+}
+
+impl AlbumTrack {
+    fn new(track: &Track) -> Option<Self> {
+        let Some(track_num) = &track.number else {
+            return None;
+        };
+
+        let Ok(parsed_track_num) = track_num.parse::<u32>() else {
+            return None;
+        };
+
+        let title = track.title.clone().unwrap_or("unknown track".to_string());
+
+        let track_len = track.length.unwrap_or(0);
+
+        Some(Self { num: parsed_track_num, title, len: track_len })
     }
 }
